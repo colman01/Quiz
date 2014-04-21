ce2e94e82e3cfe5dafbcef5c7c546668335e5948
Colman Marcus-Quinn <colman01@gmail.com>
2014-04-21 18:22:03 +0200

    x

--- VideoQuiz/PlayerViewController.m
+++ VideoQuiz/PlayerViewController.m
@@ -30,7 +30,7 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
+- (void)didReceiveMemoryWarning 
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
