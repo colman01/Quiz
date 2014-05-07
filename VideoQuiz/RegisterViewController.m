bdd7a72e2ad8d5e935a6b23c177bf90f60ab27cb
Colman Marcus-Quinn <colman01@gmail.com>
2014-05-07 18:03:21 +0200

    Navigation variant added.

--- VideoQuiz/RegisterViewController.m
+++ VideoQuiz/RegisterViewController.m
@@ -30,6 +30,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSString *str = @"http://192.168.0.105/videoQuiz/account_login_action.php";
+    NSURL *url = [NSURL URLWithString:[str stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[[NSString stringWithFormat:@"username=%@&password=%@&email=%@", userName.text, password.text, email.text] dataUsingEncoding:NSUTF8StringEncoding]];
+//    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+//    [conn start];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,11 +45,24 @@
     [super didReceiveMemoryWarning];
 }
 
+- (IBAction)registerButton:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+    
+    NSArray *viewControllers = [[self navigationController] viewControllers];
+    for (int i = 0; i < [viewControllers count]; i++){
+        id obj = [viewControllers objectAtIndex:i];
+        if ([obj isKindOfClass:[ViewController class]]){
+            [[self navigationController] popToViewController:obj animated:YES];
+            return;
+        }
+    }
+}
 
 #pragma mark - Navigation
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-}
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+//{
+//    
+//}
 
 @end
